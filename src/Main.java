ea4d9822d0e1923dc073cc3f5d0b68daca684b7e
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-18 12:03:55 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by KPS on 7/18/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("omid");
+        stringList.add("ali");
+        stringList.add("mehdi");
+        System.out.println(stringList.get(1));
+    }
+}
